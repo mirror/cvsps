8ec4b1ce8ad7287f005804e5d5bf56ba777455a6
Eric S. Raymond <esr@thyrsus.com>
2012-12-24 14:00:39 -0500

    The -A option goes away.

--- cvsps.c
+++ cvsps.c
@@ -101,7 +101,6 @@ static int restrict_tag_ps_end = INT_MAX;
 static const char * diff_opts;
 static int compress;
 static char compress_arg[8];
-static bool track_branch_ancestry;
 static time_t regression_time;
 
 static int parse_args(int, char *[]);
@@ -136,7 +135,6 @@ static void set_psm_initial(PatchSetMember * psm);
 static int check_rev_funk(PatchSet *, CvsFileRevision *);
 static CvsFileRevision * rev_follow_branch(CvsFileRevision *, const char *);
 static bool before_tag(CvsFileRevision * rev, const char * tag);
-static void determine_branch_ancestor(PatchSet * ps, PatchSet * head_ps);
 static void handle_collisions();
 static Branch * create_branch(const char * name) ;
 static void find_branch_points(PatchSet * ps);
@@ -651,13 +649,13 @@ static int parse_args(int argc, char *argv[])
 	    continue;
 	}
 	
-	/* git-cvsimport expects to be able to pass this */
+	/* leave this in place so git-cvsimport will cause graceful death */
 	if (strcmp(argv[i], "-u") == 0)
 	{
-	    i++;
-	    continue;
+	    fprintf(stderr, "cvsps: -u is no longer supported.\n");
+	    exit(1);
 	}
-	
+
 	if (strcmp(argv[i], "-s") == 0)
 	{
 	    PatchSetRange * range;
@@ -820,13 +818,6 @@ static int parse_args(int argc, char *argv[])
 	if (strcmp(argv[i], "-h") == 0)
 	    return usage(NULL, NULL);
 
-	/* git-cvsimport expect to be able to pass this */
-	if (strcmp(argv[i], "--norc") == 0)
-	{
-	    i++;
-	    continue;
-	}
-
 	if (strcmp(argv[i], "--test-log") == 0)
 	{
 	    if (++i >= argc)
@@ -884,13 +875,6 @@ static int parse_args(int argc, char *argv[])
 		return usage("argument to --root missing", "");
 
 	    strcpy(root_path, argv[i++]);
-	    continue;
-	}
-
-	if (strcmp(argv[i], "-A") == 0)
-	{
-	    track_branch_ancestry = true;
-	    i++;
 	    continue;
 	}
 
@@ -1755,21 +1739,8 @@ static void assign_patchset_id(PatchSet * ps)
     {
 	ps_counter++;
 	ps->psid = ps_counter;
-	
-	if (track_branch_ancestry && strcmp(ps->branch, "HEAD") != 0)
-	{
-	    PatchSet * head_ps = (PatchSet*)get_hash_object(branch_heads, ps->branch);
-	    if (!head_ps) 
-	    {
-		head_ps = ps;
-		put_hash_object(branch_heads, ps->branch, head_ps);
-	    }
-	    
-	    determine_branch_ancestor(ps, head_ps);
-	}
 
 	find_branch_points(ps);
-
     }
     else
     {
@@ -2789,73 +2760,6 @@ static CvsFileRevision * rev_follow_branch(CvsFileRevision * rev, const char * b
     }
     
     return NULL;
-}
-
-static void determine_branch_ancestor(PatchSet * ps, PatchSet * head_ps)
-{
-    struct list_head * next;
-    CvsFileRevision * rev;
-
-    /* PatchSet 1 has no ancestor */
-    if (ps->psid == 1)
-	return;
-
-    /* HEAD branch patchsets have no ancestry, but callers should know that */
-    if (strcmp(ps->branch, "HEAD") == 0)
-    {
-	debug(DEBUG_APPWARN, "WARNING: no branch ancestry for HEAD");
-	return;
-    }
-
-    for (next = ps->members.next; next != &ps->members; next = next->next) 
-    {
-	PatchSetMember * psm = list_entry(next, PatchSetMember, link);
-	rev = psm->pre_rev;
-	int d1, d2;
-
-	/* the reason this is at all complicated has to do with a 
-	 * branch off of a branch.  it is possible (and indeed 
-	 * likely) that some file would not have been modified 
-	 * from the initial branch point to the branch-off-branch 
-	 * point, and therefore the branch-off-branch point is 
-	 * really branch-off-HEAD for that specific member (file).  
-	 * in that case, rev->branch will say HEAD but we want 
-	 * to know the symbolic name of the first branch
-	 * so we continue to look member after member until we find
-	 * the 'deepest' branching.  deepest can actually be determined
-	 * by considering the revision currently indicated by 
-	 * ps->ancestor_branch (by symbolic lookup) and rev->rev. the 
-	 * one with more dots wins
-	 *
-	 * also, the first commit in which a branch-off-branch is 
-	 * mentioned may ONLY modify files never committed since
-	 * original branch-off-HEAD was created, so we have to keep
-	 * checking, ps after ps to be sure to get the deepest ancestor
-	 *
-	 * note: rev is the pre-commit revision, not the post-commit
-	 */
-	if (!head_ps->ancestor_branch)
-	    d1 = -1;
-	else if (strcmp(ps->branch, rev->branch) == 0)
-	    continue;
-	else if (strcmp(head_ps->ancestor_branch, "HEAD") == 0)
-	    d1 = 1;
-	else {
-	    /* branch_rev may not exist if the file was added on this branch for example */
-	    const char * branch_rev = (char *)get_hash_object(rev->file->branches_sym, head_ps->ancestor_branch);
-	    d1 = branch_rev ? count_dots(branch_rev) : 1;
-	}
-	
-	/* HACK: we sometimes pretend to derive from the import branch.  
-	 * just don't do that.  this is the easiest way to prevent... 
-	 */
-	d2 = (strcmp(rev->rev, "1.1.1.1") == 0) ? 0 : count_dots(rev->rev);
-	
-	if (d2 > d1)
-	    head_ps->ancestor_branch = rev->branch;
-
- 	//printf("-----> %d ancestry %s %s %s\n", ps->psid, ps->branch, head_ps->ancestor_branch, rev->file->filename);
-    }
 }
 
 static void handle_collisions()
