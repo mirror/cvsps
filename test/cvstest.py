a5fa556232198ee1006da0f9dc5453305ce0d192
Eric S. Raymond <esr@thyrsus.com>
2012-12-26 10:46:30 -0500

    Make it possible to pass verbose into the convert method.

--- test/cvstest.py
+++ test/cvstest.py
@@ -75,8 +75,9 @@ class CVSRepository:
         return self.checkouts[-1]
     def convert(self, module, gitdir):
         "Convert a specified module"
+        vopt = "-v " * verbose
         do_or_die("rm -fr {0} && mkdir {0} && git init {0}".format(gitdir))
-        do_or_die('cvsps --root ":local:{0}" --fast-export {1} | (cd {2}; git fast-import --quiet --done && git checkout)'.format(self.directory, module, gitdir))
+        do_or_die('cvsps {3} --root ":local:{0}" --fast-export {1} | (cd {2}; git fast-import --quiet --done && git checkout)'.format(self.directory, module, gitdir, vopt))
     def cleanup(self):
         "Clean up the repository checkout directories."
         if not self.retain:
