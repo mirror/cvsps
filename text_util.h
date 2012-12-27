777abc7d39f0ed292fad5d3ac7811977a7d8e976
Eric S. Raymond <esr@thyrsus.com>
2012-12-27 13:43:34 -0500

    Remove dead code.

--- text_util.h
+++ text_util.h
@@ -18,23 +18,6 @@ extern "C"
 #endif
 
 char*       chop( char* src );
-char*       digits( char* src );
-char*       lower_case( char* src );
-char*       reverse( char* src );
-char*       trim( char* src );
-void        trim_zeros_after_decimal( char* src );
-char*       upper_case( char* src );
-int         strrcmp( const char* haystack, const char* needle );
-
-const char* cents2money( long cents );
-long        money2cents( const char* money );
-
-// these two allocate returned memory, so be sure to free it...
-char*       frobstr( char* src );
-char*       unfrobstr( char* src );
-
-void        str2hex( char* dest, const char* src, int slen );
-void        hex2str( char* dest, const char* src, int slen );
 
 #ifdef __cplusplus
 }
